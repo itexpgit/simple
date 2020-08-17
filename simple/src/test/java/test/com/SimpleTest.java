b18cc95d8b78838f6ff5bfd40beb51b468434bd3
itexpgit <66497358+itexpgit@users.noreply.github.com>
2020-08-16 21:20:55 -0500

    new project

---
+++ simple/src/test/java/test/com/SimpleTest.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.com;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ *
+ * @author Uma
+ */
+public class SimpleTest {
+    
+    
+    
+    public SimpleTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+        
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void hello() {
+        System.out.println("Test Started");
+        System.setProperty("webdriver.chrome.driver", "C:\\Data\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+System.out.println("Test Starting");
+driver.get("https://www.google.com");
+System.out.println("Google Launched");
+driver.quit();
+
+     
+    }
+}
